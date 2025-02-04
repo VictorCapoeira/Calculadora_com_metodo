3f9ea95b22056d290a69294fb3027d7c06af703e
Allan Gabriel <Victor>
2025-02-04 10:58:47 -0300

    Criada classe

--- calculadora/Menu.cs
+++ calculadora/Menu.cs
@@ -1,5 +1,10 @@
 namespace Menu{
-    public class Escolha(){
-        
+    public class Escolha{
+        public static string Imprimir(){
+            string option = "";
+            do{
+                Console.WriteLine("Opção 01")
+            }
+        }
     }
 }
