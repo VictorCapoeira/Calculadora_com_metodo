8a25ddc9c0a99236b9a58fa8231f71720bbc0793
Victor <victorgit28@outlook.com>
2025-02-04 11:21:02 -0300

    Criado classe para contas

---
+++ calculadora/Calculadora.cs
@@ -0,0 +1,24 @@
+namespace Calculadora
+{
+    public class Contas()
+    {
+        public static float Soma(float a, float b)
+        {
+            return a + b;
+        }
+        public static float Subtracao(float a, float b)
+        {
+            return a - b;
+        }
+        public static float Multiplicacaoo(float a, float b)
+        {
+            return a * b;
+        }
+        public static float Divisao(float a, float b)
+        {
+            return a / b;
+        }
+        
+    }
+
+}
