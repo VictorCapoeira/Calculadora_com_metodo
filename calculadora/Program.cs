c73b978bda7a13f22dc82caa1571b2c751c62975
Victor <victorgit28@outlook.com>
2025-02-05 08:27:57 -0300

    Ajustado texto de exibição

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -8,14 +8,17 @@ string operacao;
 Console.WriteLine("Calculadora");
 Console.WriteLine("Selecione a operaçã: ");
 operacao = Escolha.Imprimir();
-
-Console.WriteLine("a " + operacao + " b = a"+operacao+"b");
+Console.Clear();
+Console.WriteLine("------------------------------------------------------------------------");
+Console.WriteLine("a " + operacao + " b = a" + operacao + "b");
 Console.WriteLine("Informe o primeiro valor");
 valor_a = float.Parse(Console.ReadLine());
+Console.WriteLine("------------------------------------------------------------------------");
 Console.WriteLine(valor_a + " " + operacao + " b = " + valor_a + operacao + "b");
 Console.WriteLine("Informe o segundo valor");
 valor_b = float.Parse(Console.ReadLine());
-Console.WriteLine(valor_a + " " + operacao + valor_b +"  = " + valor_a + operacao + valor_b);
+Console.WriteLine(valor_a + " " + operacao + " " + valor_b + "  = " + valor_a + operacao + valor_b);
+Console.WriteLine("------------------------------------------------------------------------");
 
 if (operacao == "5")
 {
@@ -26,16 +29,16 @@ else
     switch (operacao)
     {
         case "+":
-            Console.WriteLine(valor_a + " " + operacao + valor_b +"  = " + Contas.Soma(valor_a, valor_b));
+            Console.WriteLine(valor_a + " " + operacao + " " + valor_b + "  = " + Contas.Soma(valor_a, valor_b));
             break;
         case "-":
-            Console.WriteLine(valor_a + " " + operacao + valor_b +"  = " + Contas.Subtracao(valor_a, valor_b));
+            Console.WriteLine(valor_a + " " + operacao + " " + valor_b + "  = " + Contas.Subtracao(valor_a, valor_b));
             break;
         case "*":
-            Console.WriteLine(valor_a + " " + operacao + valor_b +"  = " + Contas.Multiplicacaoo(valor_a, valor_b));
+            Console.WriteLine(valor_a + " " + operacao + " " + valor_b + "  = " + Contas.Multiplicacaoo(valor_a, valor_b));
             break;
         case "/":
-            Console.WriteLine(valor_a + " " + operacao + valor_b +"  = " + Contas.Divisao(valor_a, valor_b));
+            Console.WriteLine(valor_a + " " + operacao + " " + valor_b + "  = " + Contas.Divisao(valor_a, valor_b));
             break;
         default:
             Console.WriteLine("Error!");
