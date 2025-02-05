e958cb9f352d0b2eec4c7322f5c615168dee4cac
Victor <victorgit28@outlook.com>
2025-02-05 07:58:33 -0300

    Código para coletar valores da variavel a e b

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -1,6 +1,14 @@
 ﻿using Menu;
 using Calculadora;
 
+float valor_a;
+float valor_b;
+string operacao;
+
 Console.WriteLine("Calculadora");
 Console.WriteLine("Selecione a operaçã: ");
-string operacao = Menu.Escolha.Imprimir();
+operacao = Menu.Escolha.Imprimir();
+Console.WriteLine("Informe o primeiro valor");
+valor_a = float.Parse(Console.ReadLine());
+Console.WriteLine("Informe o segundo valor");
+valor_b = float.Parse(Console.ReadLine());
