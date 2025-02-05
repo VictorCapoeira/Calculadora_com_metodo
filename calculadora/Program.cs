8e124fd6664a6c7d76f4d8bfc70155923b87bd9f
Victor <victorgit28@outlook.com>
2025-02-05 08:00:14 -0300

    Código para realizar as operações

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -8,7 +8,27 @@ string operacao;
 Console.WriteLine("Calculadora");
 Console.WriteLine("Selecione a operaçã: ");
 operacao = Menu.Escolha.Imprimir();
+
 Console.WriteLine("Informe o primeiro valor");
 valor_a = float.Parse(Console.ReadLine());
 Console.WriteLine("Informe o segundo valor");
 valor_b = float.Parse(Console.ReadLine());
+
+switch (operacao)
+{
+    case "1":
+        Calculadora.Contas.Soma(valor_a, valor_b);
+        break;
+    case "2":
+        Calculadora.Contas.Soma(valor_a, valor_b);
+        break;
+    case "3":
+        Calculadora.Contas.Soma(valor_a, valor_b);
+        break;
+    case "4":
+        Calculadora.Contas.Soma(valor_a, valor_b);
+        break;
+    default:
+        Console.WriteLine("Error!");
+        break;
+}
