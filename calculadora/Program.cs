2c85965f9228c74f5dfa7ea9b698e09ecfe21617
Victor <victorgit28@outlook.com>
2025-02-06 08:25:11 -0300

    Loop adicionado

--- calculadora/Program.cs
+++ calculadora/Program.cs
@@ -3,34 +3,36 @@ using Calculadora;
 
 float valor_a;
 float valor_b;
-string operacao;
+string operacao = "";
 
 Console.WriteLine("Calculadora");
-Console.WriteLine("Selecione a operaçã: ");
-operacao = Escolha.Imprimir();
-Console.Clear();
+while (operacao != "s")
+{
+    Console.WriteLine("Selecione a operaçã: ");
+    operacao = Escolha.Imprimir();
+    Console.Clear();
 
 
-if (operacao == "5")
-{
-    Console.WriteLine("Programa encerrado!");
-}
-else
-{
-    switch(operacao){
-        case "+":
-            Contas.Soma();
-        break;
-        case "-":
-            Contas.Subtracao();
-        break;
-        case "*":
-            Contas.Multiplicacaoo();
-        break;
-        case "/":
-            Contas.Divisao();
-        break;
-}
+    if (operacao == "5")
+    {
+        Console.WriteLine("Programa encerrado!");
     }
-    
-
+    else
+    {
+        switch (operacao)
+        {
+            case "+":
+                Contas.Soma();
+                break;
+            case "-":
+                Contas.Subtracao();
+                break;
+            case "*":
+                Contas.Multiplicacaoo();
+                break;
+            case "/":
+                Contas.Divisao();
+                break;
+        }
+    }
+}
