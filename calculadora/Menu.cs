3d81b9b43207be85085920501503520e8ebaf8a6
Victor <victorgit28@outlook.com>
2025-02-06 08:42:18 -0300

    Adicionado metodos de cores no lugar do witriline

--- calculadora/Menu.cs
+++ calculadora/Menu.cs
@@ -1,31 +1,33 @@
+using Layout;
+
 namespace Menu{
     public class Escolha{
         public static string Imprimir(){
             string option = "";
             do{
-                Console.WriteLine("[Adição] -> +");
-                Console.WriteLine("[Subtração] -> -");
-                Console.WriteLine("[Multiplicação] -> *");
-                Console.WriteLine("[Divisão] -> /");
-                Console.WriteLine("[Sair] -> s");
+                Cores.Amarelo("[Adição] -> +");
+                Cores.Amarelo("[Subtração] -> -");
+                Cores.Amarelo("[Multiplicação] -> *");
+                Cores.Amarelo("[Divisão] -> /");
+                Cores.Amarelo("[Sair] -> s");
                 return option = Console.ReadLine();
             }while(option != "s");
         }
         public static float Obter_valor(){
             while(true){
-                Console.WriteLine("Informe o valor: ");
+                Cores.Amarelo("Informe o valor: ");
                 if(float.TryParse(Console.ReadLine(), out float valor_x)){
                     
                     return valor_x;
                 }
                 else
                 {
-                    Console.WriteLine("Valor inválido!");
+                    Cores.Vermelho("Valor inválido!");
                 }
             }
         }
         public static void Exibir_valores(float valor_a, float valor_b, string valor_op, float resultado){
-            Console.WriteLine(valor_a + " " + valor_op + " " + valor_b + "  = " + resultado);
+            Cores.Verde(valor_a + " " + valor_op + " " + valor_b + "  = " + resultado);
         }
     }
 }
