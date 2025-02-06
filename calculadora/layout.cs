d4db18d13d8a2d08d52fc33ae308d29ffa2b121f
Victor <victorgit28@outlook.com>
2025-02-06 08:37:32 -0300

    Criado classe vermelho

--- calculadora/layout.cs
+++ calculadora/layout.cs
@@ -2,7 +2,9 @@ namespace Layout
 {
     public class Cores{
         public static void Vermelho(string texto){
-            
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(texto);
+            Console.ResetColor();
         }
     }
 }
